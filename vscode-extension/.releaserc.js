f0d8ef3033f383a01b483ba6f1acbbc4b571048a
bitkojine <74838686+bitkojine@users.noreply.github.com>
2026-01-27 02:27:47 +0200

    ci: remove git and changelog plugins to skip pushing to protected branch

--- vscode-extension/.releaserc.js
+++ vscode-extension/.releaserc.js
@@ -9,7 +9,6 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
     [
       '@semantic-release/npm',
       {
@@ -26,13 +25,6 @@ module.exports = {
             label: 'VSIX Extension'
           }
         ]
-      }
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
     ]
   ],
